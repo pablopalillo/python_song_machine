919507174e43113ce66c18a191ffce961b940318
pablopalillo <pablo.576@hotmail.com>
2021-12-23 15:54:01 -0500

    Python TDD add principals tests.

--- imports/tests.py
+++ imports/tests.py
@@ -1,3 +1,38 @@
 from django.test import TestCase
 
-# Create your tests here.
+
+class TestServicesImport(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        pass
+
+    def test_service_get_json(self):
+        """ Test json URL valid.
+        """
+        pass
+
+    def test_service_not_valid_json(self):
+        """ Test json with URL not valid.
+        """
+        pass
+
+    def test_service_import_json_to_dic(self):
+        """ Test return service convert the json response to dic python format
+        """
+        pass
+
+    def test_service_import_genres(self):
+        """ Test import genres data
+        """
+        pass
+
+    def test_service_import_artists(self):
+        """ Test import artist data
+        """
+        pass
+
+    def test_service_import_songs(self):
+        """ Test import songs data
+        """
+        pass
