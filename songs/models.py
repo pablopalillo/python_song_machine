7091bb96e70e89a838a53135aac6c66a50267264
pablopalillo <pablo.576@hotmail.com>
2021-12-22 22:30:22 -0500

    Song Models added:
        - Model Genre.
        - Model Artist.
        - Model Song with Many to Many relation.

--- songs/models.py
+++ songs/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Genre(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=35)
+
+    def __str__(self):
+        return self.name
+
+
+class Artist(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Song(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    release_date = models.DateField()
+    explicit = models.BooleanField(null=True)
+    artists = models.ManyToManyField(Artist)
+    genres = models.ManyToManyField(Genre)
