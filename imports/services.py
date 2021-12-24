96e58ff85174135d6f2baf5889f2eb59e9b2d392
pablopalillo <pablo.576@hotmail.com>
2021-12-23 21:46:26 -0500

    Import services and test created.

--- imports/services.py
+++ imports/services.py
@@ -36,7 +36,7 @@ def get_last_song(limit: int):
     """ Return top song
 
     :param limit: Cuantity of songs
-    :return: Dic of songs
+    :return: list of songs
     """
 
     list_songs = get_songs()
@@ -44,3 +44,95 @@ def get_last_song(limit: int):
         return list_songs[:limit]
     else:
         return None
+
+
+def get_genres_data(last_song: list):
+
+    list_genders = []
+    for song in last_song:
+
+        genres = song.get('genres')
+
+        for genre in genres:
+            genre_id = genre.get('genreId')
+            genre_name = genre.get('name')
+
+            if str(genre_id) not in list_genders:
+                list_genders.append({str(genre_id): genre_name})
+
+    return list_genders
+
+
+def get_artist_data(last_song: list):
+
+    list_artist = []
+    for song in last_song:
+
+        name_artist = song.get('artistName')
+        id_artist = song.get('artistId')
+
+        #  for Database simplification cut the origin id for shorter ID
+        id_artist = id_artist[:4]
+
+        if str(id_artist) not in list_artist:
+            list_artist.append({str(id_artist): name_artist})
+
+    return list_artist
+
+
+def get_songs_data(last_song: list):
+
+    list_songs = []
+    for song in last_song:
+
+        title = song.get('artistName')
+        release_date = song.get('releaseDate')
+        is_explicit = song.get('contentAdvisoryRating')
+
+        if is_explicit:
+            is_explicit = True
+        else:
+            is_explicit = False
+
+        artist_id = song.get('artistId')
+        if artist_id:
+            #  for Database simplification cut the origin id for shorter ID
+            artist_id = artist_id[:4]
+
+        genres = []
+        for genre in song.get('genres'):
+            genres.append(genre.get('genreId'))
+
+        list_songs.append(
+            {
+                'title': title,
+                'release_date': release_date,
+                'explicit': is_explicit,
+                'artist_id': artist_id,
+                'genres': genre
+            }
+        )
+
+    return list_songs
+
+
+def get_list_data():
+    """ get list of dict imported data
+
+    :return: List
+    """
+    last_songs = get_last_song(50)
+    songs_format_data = {}
+
+    if last_songs:
+        gender_data = get_genres_data(last_songs)
+        artist_data = get_artist_data(last_songs)
+        songs_data = get_songs_data(last_songs)
+
+        songs_format_data = {
+            'genres': gender_data,
+            'artists': artist_data,
+            'songs': songs_data
+        }
+
+    return songs_format_data
--- imports/tests.py
+++ imports/tests.py
@@ -1,10 +1,62 @@
 from django.test import TestCase
-from .services import import_json, get_songs, get_last_song
+from .services import import_json, get_songs, get_last_song, get_genres_data, get_artist_data, get_songs_data
 
 
 class TestServicesImport(TestCase):
 
     url_song = "https://rss.applemarketingtools.com/api/v2/us/music/most-played/100/songs.json"
+    song_data = []
+
+    def setUp(self):
+        self.song_data = [
+                {
+                    "artistName": "Kodak Black",
+                    "id": "1592774398",
+                    "name": "Super Gremlin",
+                    "releaseDate": "2021-10-30",
+                    "kind": "songs",
+                    "artistId": "953921140",
+                    "artistUrl": "https://music.apple.com/us/artist/kodak-black/953921140",
+                    "contentAdvisoryRating": "Explict",
+                    "artworkUrl100": "https://is2-ssl.mzstatic.com/image/thumb/Music126/v4/6b/3b/fc/6b3bfc34-cdce-b3ef-db0f-caa4b760dd0b/075679764195.jpg/100x100bb.jpg",
+                    "genres": [
+                        {
+                            "genreId": "18",
+                            "name": "Hip-Hop/Rap",
+                            "url": "https://itunes.apple.com/us/genre/id18"
+                        },
+                        {
+                            "genreId": "34",
+                            "name": "Music",
+                            "url": "https://itunes.apple.com/us/genre/id34"
+                        }
+                    ],
+                    "url": "https://music.apple.com/us/album/super-gremlin/1592774394?i=1592774398"
+                },
+                {
+                    "artistName": "Lil Durk",
+                    "id": "1600722156",
+                    "name": "Broadway Girls (feat. Morgan Wallen)",
+                    "releaseDate": "2021-12-19",
+                    "kind": "songs",
+                    "artistId": "541282483",
+                    "artistUrl": "https://music.apple.com/us/artist/lil-durk/541282483",
+                    "artworkUrl100": "https://is4-ssl.mzstatic.com/image/thumb/Music116/v4/6f/73/de/6f73de5d-9bd5-05f1-ebbc-0e053afea88d/886449812868.jpg/100x100bb.jpg",
+                    "genres": [
+                        {
+                            "genreId": "18",
+                            "name": "Hip-Hop/Rap",
+                            "url": "https://itunes.apple.com/us/genre/id18"
+                        },
+                        {
+                            "genreId": "34",
+                            "name": "Music",
+                            "url": "https://itunes.apple.com/us/genre/id34"
+                        }
+                    ],
+                    "url": "https://music.apple.com/us/album/broadway-girls-feat-morgan-wallen/1600721938?i=1600722156"
+                }
+            ]
 
     def test_service_get_json(self):
         """ Test json URL valid.
@@ -23,7 +75,7 @@ class TestServicesImport(TestCase):
         """ Test return service convert the json response to dic python format
         """
         data_dic_test = get_songs()
-        self.assertTrue(isinstance(data_dic_test, list), "data is not a dict.")
+        self.assertTrue(isinstance(data_dic_test, list), "data is not a list.")
 
     def test_service_import_top_song(self):
         """ Test return service convert the json response to dic python format
@@ -35,14 +87,17 @@ class TestServicesImport(TestCase):
     def test_service_import_genres(self):
         """ Test import genres data
         """
-        pass
+        genders = get_genres_data(self.song_data)
+        self.assertTrue(isinstance(genders, list), "data is not a list.")
 
     def test_service_import_artists(self):
         """ Test import artist data
         """
-        pass
+        artists = get_artist_data(self.song_data)
+        self.assertTrue(isinstance(artists, list), "data is not a list.")
 
     def test_service_import_songs(self):
         """ Test import songs data
         """
-        pass
+        songs = get_songs_data(self.song_data)
+        self.assertTrue(isinstance(songs, list), "data is not a list.")
